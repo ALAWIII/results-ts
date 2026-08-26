d8fdcfaaf44d80df687640a3c3c9dc7ae852163f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-26 16:12:49 +0300

    test(result): add unwrapOr tests for Ok and Err.

---
+++ test/result/unwrap/unwrap-or.test.ts
@@ -0,0 +1,16 @@
+import { Err, Ok } from '../../../src';
+
+describe('Ok.unwrapOr()', () => {
+    test('it should return the original value of an Ok and discards the provided one.', () => {
+        const ok = Ok(312).unwrapOr(234);
+        expect(ok).toBe(312);
+        expect(ok).not.toBe(234);
+    });
+});
+describe('Err.unwrapOr()', () => {
+    test('it should return the provided value as fallback.', () => {
+        const err = Err<number, number>(32).unwrapOr(234);
+        expect(err).toBe(234);
+        expect(err).not.toBe(32);
+    });
+});
