d345be5660cfb359bf73b5db742c154b867b1529
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-27 12:34:40 +0300

    test(result): add iterable tests for Ok and Err.

---
+++ test/result/iterable.test.ts
@@ -0,0 +1,15 @@
+import { expect_never } from 'util';
+import { Err, Ok } from '../../src';
+
+test('Ok.iterable', () => {
+    expect(Array.from(Ok('hello'))).toEqual(['hello']);
+    expect(Array.from(Ok([1, 2, 3]))).toEqual([[1, 2, 3]]);
+    expect(Array.from(Ok(1))).toEqual([1]);
+});
+
+test('Err.iterable', () => {
+    for (const item of Err([123])) {
+        expect_never(item, true);
+        throw Error('Unreachable, Err@@iterator should emit no value and return');
+    }
+});
